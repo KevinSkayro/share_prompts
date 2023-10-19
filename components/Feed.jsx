8f8d0593ee030e0287ec4b5998877b044bc74286
Kevin Castro <kev1n-1997@outlook.com>
2023-10-19 10:52:09 -0400

    added new route to request prompts in HP
    added prompt card code to show prompts data
    Updated Feed to show prompt cards
    added API route to request prompt card data

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from 'react';
+import PromptCard from '@components/PromptCard';
+
+const PromptCardList = ({data, handleTagClick}) => {
+    console.log(data)
+    return (
+        <div className='mt-16 prompt_layout'>
+            {data.map((prompt) => (
+                <PromptCard 
+                    key={prompt._id}
+                    post={prompt}
+                    handleTagClick={handleTagClick}
+                />
+            ))}
+        </div>
+    )
+}
 
 const Feed = () => {
+    const [searchText, setSearchText] = useState('');
+    const [prompts, setPrompts] = useState([]);
+    const handleSearchChange = (e) => {
+
+    }
+
+    useEffect(() => {
+        const fetchPrompts = async () => {
+            const response = await fetch('/api/prompt');
+            const data = await response.json();
+            console.log(data);
+            setPrompts(data);
+        }
+        fetchPrompts();
+    }, []);
   return (
-    <div>Feed</div>
+    <section className='feed'>
+        <form className='relative w-full flex-center'>
+            <input className='search_input peer' 
+                type="text"
+                placeholder='Search for tag or username'
+                value={searchText}
+                onChange={handleSearchChange} 
+                required
+            />
+        </form>
+
+        <PromptCardList
+            data={prompts}
+            handleTagClick={() => {}}
+        />
+    </section>
   )
 }
 
