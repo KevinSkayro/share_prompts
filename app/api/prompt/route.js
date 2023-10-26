0e36bbcd5bfec6831416446f9bcf6412a6609a75
Kevin Castro <kev1n-1997@outlook.com>
2023-10-26 11:05:21 -0400

    break cache?

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,18 @@
 import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
-export const GET = async (req) => { 
-
+export const GET = async (req) => {
     try {
-        await connectToDatabase();    
+        await connectToDatabase();
         const prompts = await Prompt.find({}).populate('creator');
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompts), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache, no-store, must-revalidate',
+            },
+        });
     } catch (error) {
-        return new Response(JSON.stringify(error), { status: 500 })
+        return new Response(JSON.stringify(error), { status: 500 });
     }
-
 }
