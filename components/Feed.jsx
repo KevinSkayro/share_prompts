0d77d83a5012d3c401f7481ba025714ee780f475
Kevin Castro <kev1n-1997@outlook.com>
2023-10-26 09:37:33 -0400

    Update Feed.jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,6 @@
 'use client';
-
 import { useState, useEffect } from 'react';
 import PromptCard from '@components/PromptCard';
-
 const PromptCardList = ({data, handleTagClick}) => {
     return (
         <div className='mt-16 prompt_layout'>
@@ -16,52 +14,36 @@ const PromptCardList = ({data, handleTagClick}) => {
         </div>
     )
 }
-
 const Feed = () => {
     const [searchText, setSearchText] = useState('');
     const [prompts, setPrompts] = useState([]);
     const handleSearchChange = (e) => {
-        // Handle search input change here
     }
-
     const fetchPrompts = async () => {
-        try {
-            const response = await fetch('/api/prompt');
-            const data = await response.json();
-            setPrompts(data);
-        } catch (error) {
-            // Handle fetch error
-            console.log(error);
-        }
+        const response = await fetch('/api/prompt');
+        const data = await response.json();
+        setPrompts(data);
     };
-
     useEffect(() => {
         fetchPrompts();
     }, []);
     
-    return (
-        <section className='feed'>
-            <form className='relative w-full flex-center'>
-                <input
-                    className='search_input peer' 
-                    type="text"
-                    placeholder='Search for tag or username'
-                    value={searchText}
-                    onChange={handleSearchChange}
-                    required
-                />
-            </form>
-
-            {prompts.length > 0 ? (
-                <PromptCardList
-                    data={prompts}
-                    handleTagClick={() => {}}
-                />
-            ) : (
-                <div>Loading or no data available.</div>
-            )}
-        </section>
-    );
+  return (
+    <section className='feed'>
+        <form className='relative w-full flex-center'>
+            <input className='search_input peer' 
+                type="text"
+                placeholder='Search for tag or username'
+                value={searchText}
+                onChange={handleSearchChange} 
+                required
+            />
+        </form>
+        <PromptCardList
+            data={prompts}
+            handleTagClick={() => {}}
+        />
+    </section>
+  )
 }
-
-export default Feed;
+export default Feed
