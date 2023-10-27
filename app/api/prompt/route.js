792792c45a5d2c25910952715e7ff78b645201ac
Kevin Castro <kev1n-1997@outlook.com>
2023-10-27 10:26:01 -0400

    cache

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,19 @@
 import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
 export const revalidate = 10;
-export const GET = async (req) => {
+export const GET = async (req, res) => {
     try {
         await connectToDatabase();
         const prompts = await Prompt.find({}).populate('creator');
 
-        return new Response(JSON.stringify(prompts), {status: 200});
+        return new Response(JSON.stringify(prompts), {
+            status: 200,
+            headers: {
+                'Cache-Control': 's-maxage=1, stale-while-revalidate'
+            }
+        });
     } catch (error) {
         return new Response(JSON.stringify(error), { status: 500 });
     }
+
 }
--- app/api/users/[id]/prompts/route.js
+++ app/api/users/[id]/prompts/route.js
@@ -1,16 +1,13 @@
 import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
-export const GET = async (req, res,  {params}) => { 
+export const GET = async (req,  {params}) => { 
     try {
         await connectToDatabase();    
         const prompts = await Prompt.find({
             creator: params.id
         }).populate('creator');
-
-        res = new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
-        res = new Response(JSON.stringify(error), { status: 500 })
+        return new Response(JSON.stringify(error), { status: 500 })
     }
-    res.setHeader('Cache-Control: s-maxage=1, stale-while-revalidate');
-    return res;
 }
