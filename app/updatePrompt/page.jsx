9e2e93f8b96a36d22b353c6bdc811f6e07285460
Kevin Castro <kev1n-1997@outlook.com>
2023-10-30 18:35:56 -0400

    fixed small mistake in imports

--- app/updatePrompt/page.jsx
+++ app/updatePrompt/page.jsx
@@ -1,6 +1,6 @@
 'use client';
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
